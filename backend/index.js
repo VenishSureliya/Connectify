e1fb005ef34b5f49da8a7e4766b3e74fe3c0c084
Venish <venishsureliya@gmail.com>
2024-08-14 00:23:59 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,33 @@
 const express = require("express")
-const mongoose = require("mongoose")
+const { connectToDatabase } = require("./database")
+// const mongoose = require("mongoose")
 
-mongoose.connect(
-    "mongodb+srv://venishsureliya:capstonevenish@connectify01.qfoxi.mongodb.net/?retryWrites=true&w=majority&appName=Connectify01"
-).then(
-    () => {
-        console.log("Connected!")
-    }
-).catch(
-    () => {
-        console.log("Failed!")
-    }
-)
+// mongoose.connect(
+//     "mongodb+srv://venishsureliya:capstonevenish@connectify01.qfoxi.mongodb.net/?retryWrites=true&w=majority&appName=Connectify01"
+// ).then(
+//     () => {
+//         console.log("Connected!")
+//     }
+// ).catch(
+//     () => {
+//         console.log("Failed!")
+//     }
+// )
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
-app.get(
-    "/",
-    (req, res) => {
-        res.send("I'm response!!")
-    }
-)
+app.use(express.json())
 
-app.listen(
-    5000,
-    () => console.log("Backend is running!")
-)
+connectToDatabase().then(() => {
+    app.listen(port, () => {
+        console.log('Server is running on port', port)
+    })
+}).catch((error) => {
+    console.error("Failed to start the server", error)
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!")
+})
